da455494fb5cb4e0f1f10bf5dba33fb0bc6db37e
sadafkhan0107 <66263424+sadafkhan0107@users.noreply.github.com>
2023-06-03 20:44:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from './components/Button/Button';
+import { Navbar} from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,3 @@
+export const Navbar = () => {
+    return <h1> Hello from Navbar </h1>
+}
